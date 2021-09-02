1f5894dc39b10d039f04d7efaf695c2c6776a2d7
bj <bj@bjyurkovich.com>
2021-09-02 09:46:40 -0400

    deploy token

--- dev/headless.sh
+++ dev/headless.sh
@@ -7,7 +7,7 @@ service ssh start
 virtualenv venv
 source /workspace/venv/bin/activate
 pip install -r requirements.txt
-jupyter lab --allow-root --port 8080 --NotebookApp.token='' --no-browser --ip=0.0.0.0
+jupyter lab --allow-root --port 8080 --NotebookApp.token='nhtsadev' --no-browser --ip=0.0.0.0
 
 
 while true; do
